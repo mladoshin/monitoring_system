d232d753c137107867ed82faa128a3cf4b6a0bf3
Maxim <mladoshin@mail.ru>
2023-03-23 15:41:33 +0300

    add popover component

--- webapp/src/components/ParameterMonitor/BarChart.js
+++ webapp/src/components/ParameterMonitor/BarChart.js
@@ -1,39 +1,53 @@
+import { Typography } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import React from "react";
+import CustomPopover from "../Popover";
 import "./BarChart.scss";
 
 function BarChart({ data = [] }) {
   return (
     <Stack direction="row" spacing={5} justifyContent="center" py={1}>
-      {data.map((el) => (
-        <Bar value={el / 10 * 10000}/>
+      {data.map((el, idx) => (
+        <Bar value={(el / 10) * 10000} id={idx} />
       ))}
     </Stack>
   );
 }
 
-function Bar({value}) {
-  let color = 'green'
+function Bar({ value, id }) {
+  let color = "green";
 
-  if(value > 1.0){
-    value = 1.0
-    color = "#ff1744"
+  if (value > 1.0) {
+    value = 1.0;
+    color = "#ff1744";
   }
 
   return (
-    <Stack
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      sx={{
-        borderTop: "1px solid black",
-        borderBottom: "1px solid black",
-        height: 50,
-        width: 15,
-      }}
+    <CustomPopover
+      component={
+        <Box sx={{ p: 1 }}>
+          <Typography sx={{fontWeight: 'bold'}}>Канал {id + 1}</Typography>
+          <Typography>Максимум: {Number(value).toFixed(3)}</Typography>
+        </Box>
+      }
     >
-        <Box sx={{width: 10, height: `${value*100}%`}} bgcolor={color}></Box>
-    </Stack>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          borderTop: "1px solid black",
+          borderBottom: "1px solid black",
+          height: 50,
+          width: 15,
+        }}
+      >
+        <Box
+          sx={{ width: 10, height: `${value * 100}%` }}
+          bgcolor={color}
+        ></Box>
+      </Stack>
+    </CustomPopover>
   );
 }
 
---
+++ webapp/src/components/Popover/index.js
@@ -0,0 +1,50 @@
+import { Popover, Typography } from "@mui/material";
+import React from "react";
+
+function CustomPopover({ children, component, style={} }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  return (
+    <>
+      <div
+        aria-owns={open ? "mouse-over-popover" : undefined}
+        aria-haspopup="true"
+        onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}
+      >
+        {children}
+      </div>
+      <Popover
+        id="mouse-over-popover"
+        sx={{
+          pointerEvents: "none",
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus
+      >
+        {component}
+      </Popover>
+    </>
+  );
+}
+
+export default CustomPopover;
